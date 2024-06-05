027651167596ffe16a485c4c6cf23ef7fd5d22d5
griFFin <134530165+griffin3q@users.noreply.github.com>
2024-06-05 20:46:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = 'Your bot token here!'
+
+intents = discord.Intents.default()
+intents.messages = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+@bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send('pong!')
+
+bot.run(TOKEN)
